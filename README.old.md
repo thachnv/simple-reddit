9cfd30a63bed24c40775da1b56805f88550c9b77
Thach Nguyen <thachnv45@gmail.com>
2018-01-07 16:51:37 +0700

    Fix small UI bug, remove unused old readme file

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# simple-reddit
-A simple version of Reddit built with React &amp; Redux
--- src/modules/Topic/TopicItem.js
+++ src/modules/Topic/TopicItem.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 const TopicItem = ({ topic, onClick }) => (
   <div onClick={onClick} className="topic-item">
     <span className="topic-item-like">
-      <i className="fa fa-thumbs-o-up" />
+      <i className="fa fa-thumbs-o-up" />{' '}
       <span id="topic-item-like">{topic.like}</span>
     </span>
     <span className="topic-item-dislike">
-      <i className="fa fa-thumbs-o-down" />
+      <i className="fa fa-thumbs-o-down" />{' '}
       <span id="topic-item-dislike">{topic.dislike}</span>
     </span>
     <span className="topic-item-title" id="topic-item-title">
